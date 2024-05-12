ecb3309b2b1b73a83020df99f2d35ceae9ffeace
Benoit GUERIN <benoit.guerin1@free.fr>
2024-05-12 20:13:22 +0200

    Release 3.68.0.4

--- updateversion.sh
+++ updateversion.sh
@@ -3,7 +3,7 @@ set -ueo pipefail;
 
 newversion=
 while [ "${newversion}" = "" ]; do
-    read -p "What is the new Nexus OSS version (example: 3.67.1-01) ? " newversion
+    read -p "What is the new Nexus OSS version (example: 3.68.0-04) ? " newversion
 done
 
 oldversion=$(grep '<nexus.pkg.version>[0-9]' pom.xml|sed -e 's/ //g' -e 's/<\(\/\)\?nexus.pkg.version>//g')
